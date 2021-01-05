6641f672194fd409ec9daef028683e772260fc62
Vitor Hugo R. Tiburtino de Melo <51724161+Tibuh@users.noreply.github.com>
2021-01-04 22:46:02 -0300

    Atualização de código

--- arvoceBinaria.c
+++ arvoceBinaria.c
@@ -6,37 +6,49 @@
 typedef int TIPOCHAVE;
 typedef int bool;
 
-typedef struct reg {
-      TIPOCHAVE chave;
-      struct reg *esq;
-      struct reg *dir;
+typedef struct reg
+{
+    TIPOCHAVE chave;
+    struct reg *esq;
+    struct reg *dir;
 } noh; // nó
 
-typedef noh* pont;
+typedef noh *pont;
 
-pont inicializarArvore(){
-	return NULL;
+pont inicializarArvore()
+{
+    return NULL;
 }
-
 
 bool arvoreVazia(pont raiz)
 {
-	if(raiz == NULL) return true;
-	else return false;
+    if (raiz == NULL)
+        return true;
+    else
+        return false;
 }
 
-pont inserir(pont raiz, pont paiDaDireita){
+pont inserir(pont raiz, pont p)
+{
     //se a raiz for null, inserimos la
-    if(arvoreVazia(raiz)) return paiDaDireita;
-
-    //se a chave do elemento a ser inserido for menor que a da raiz, insere na subarvore a esquerda
-    if(paiDaDireita->chave < raiz->chave) raiz->esq = inserir(raiz->esq, paiDaDireita);
+    if (arvoreVazia(raiz))
+        return p;
+    else if (raiz->chave == p->chave)
+    {
+        printf("O elemento %d ja existe na arvore\n", p->chave);
+        return raiz;
+        //se a chave do elemento a ser inserido for menor que a da raiz, insere na subarvore a esquerda
+    }
+    else if (p->chave < raiz->chave)
+        raiz->esq = inserir(raiz->esq, p);
     //se nao insere na subarvore a esqueda
-    else raiz->dir = inserir(raiz->dir, paiDaDireita);
-     return raiz;    
+    else
+        raiz->dir = inserir(raiz->dir, p);
+    return raiz;
 }
 
-pont criaNovoNoh(TIPOCHAVE ch){
+pont criaNovoNoh(TIPOCHAVE ch)
+{
     //aloca um espaco na memoria
     pont novoNoh = (pont)malloc(sizeof(noh));
     //seta que o ponteido da esquerda e null
@@ -48,88 +60,144 @@ pont criaNovoNoh(TIPOCHAVE ch){
     //retorna o noh
     return novoNoh;
 }
-pont buscar(pont raiz, TIPOCHAVE chave){
+pont buscar(pont raiz, TIPOCHAVE chave)
+{
     //verifica se a raiz e null
-    if(raiz == NULL) return NULL;
+    if (raiz == NULL)
+        return NULL;
     //verifica se o valor da raiz e igual ao valor passado, se for retorna a raiz
-    if(raiz->chave == chave) return raiz;
+    if (raiz->chave == chave)
+        return raiz;
     //verifica se o valor da chave da raiz e maior que o passado, se for faz a recursividade e chama a subarvore esquerda
-    if(raiz->chave > chave) return buscar(raiz->esq, chave);
+    if (raiz->chave > chave)
+        return buscar(raiz->esq, chave);
     //retorna a arvore direita
     return buscar(raiz->dir, chave);
 }
 
-int contagemNumeroNohs(pont raiz){
-    if(!raiz) return 0;
+int contagemNumeroNohs(pont raiz)
+{
+    if (!raiz)
+        return 0;
     return contagemNumeroNohs(raiz->esq) + 1 + contagemNumeroNohs(raiz->dir);
 }
 
-//imprime raiz, esquesda direita
-void imprimePreOrdem(pont raiz){ 
-    printf("%d ",raiz->chave);
-    if(raiz->esq != NULL) imprimePreOrdem(raiz->esq);
-    if(raiz->dir != NULL) imprimePreOrdem(raiz->dir);
+//imprime raiz, esquesda, direita
+void imprimePreOrdem(pont raiz){
+    printf("<");
+    if(!arvoreVazia(raiz)) {
+
+        printf("%d ",raiz->chave);
+        imprimePreOrdem(raiz->esq);
+        imprimePreOrdem(raiz->dir);
+    }
+    printf(">");
+}
+
+int altura(pont r) {
+
+    int u, v;
+    if (r == NULL) return 0;
+    u = altura(r->esq);
+    v = altura(r->dir);
+    if (u > v) return u+1;
+    else return v+1;
 }
 
 //imprime esquerda, raiz, direita
-void imprimeOrdem(pont raiz){ 
-    if(raiz->esq != NULL) imprimeOrdem(raiz->esq);
-    printf("%d ",raiz->chave);
-    if(raiz->dir != NULL) imprimeOrdem(raiz->dir);
+void imprimeOrdem(pont raiz)
+{
+    if (raiz->esq != NULL)
+        imprimeOrdem(raiz->esq);
+    printf("%d ", raiz->chave);
+    if (raiz->dir != NULL)
+        imprimeOrdem(raiz->dir);
 }
 
 //imprime esquerda, direita, raiz
-void imprimePosOrdem(pont raiz){ 
-    if(raiz->esq != NULL) imprimePosOrdem(raiz->esq);
-    if(raiz->dir != NULL) imprimePosOrdem(raiz->dir);
-    printf("%d ",raiz->chave);
+void imprimePosOrdem(pont raiz)
+{
+    if (raiz->esq != NULL)
+        imprimePosOrdem(raiz->esq);
+    if (raiz->dir != NULL)
+        imprimePosOrdem(raiz->dir);
+    printf("%d ", raiz->chave);
 }
-pont buscaNoh(pont raiz, TIPOCHAVE ch, pont *pai){
+
+void imprime_red (pont h) {
+     pont *pilha;
+    int t;
+
+    pilha = malloc((1+altura(h)) * sizeof (pont));
+    pilha[t=0] = h;
+    while (t >= 0) {
+       h = pilha[t--];
+       if (h->dir != NULL) pilha[++t] = h->dir;
+       if (h->esq != NULL) pilha[++t] = h->esq;
+       if(h->esq == NULL) printf("%d ", h);
+
+    }
+    free(pilha); 
+}
+
+pont buscaNoh(pont raiz, TIPOCHAVE ch, pont *pai)
+{
     pont atual = raiz;
 
     *pai = NULL;
 
-    while (atual){
+    while (atual)
+    {
 
-        if(atual->chave == ch) return atual;
+        if (atual->chave == ch)
+            return atual;
 
         *pai = atual;
 
-        if(ch < atual->chave) atual = atual->esq;
-        else atual = atual->dir;
-
+        if (ch < atual->chave)
+            atual = atual->esq;
+        else
+            atual = atual->dir;
     }
 
     return NULL;
-
 }
 
-pont removeNoh(pont raiz, TIPOCHAVE ch) {
+pont removeNoh(pont raiz, TIPOCHAVE ch)
+{
     pont pai, no, paiDaDireita, maisADireita;
     //busca o noh a ser removido e marca o pai dele
     no = buscaNoh(raiz, ch, &pai);
     //verifica se o no e null
-    if(no == NULL) return raiz;
+    if (no == NULL)
+        return raiz;
 
     //verifica se ele possui apenas um descendente
-    if(!no->esq || !no->dir) {
-        if(!no->esq) maisADireita = no->dir;
-        else maisADireita = no->esq;
-    //caso tenha dois descendentes
-    }else {
+    if (!no->esq || !no->dir)
+    {
+        if (!no->esq)
+            maisADireita = no->dir;
+        else
+            maisADireita = no->esq;
+        //caso tenha dois descendentes
+    }
+    else
+    {
         //marca pai da direita no noh
         paiDaDireita = no;
-        //marca o mais a direita do esquerdo 
+        //marca o mais a direita do esquerdo
         maisADireita = no->esq;
 
         //repeticao para marcar o pai do mais a direita do elemento mais a direita da esquerda do no a ser removido
-        while(maisADireita->dir){
+        while (maisADireita->dir)
+        {
             paiDaDireita = maisADireita;
             maisADireita = maisADireita->dir;
         }
 
         //verifica se o pai mais a direita e diferente do no a ser removido
-        if(paiDaDireita != no){
+        if (paiDaDireita != no)
+        {
             //faz o pai dda direita apontas para o filho mais a direita
             paiDaDireita->dir = maisADireita->esq;
             //aponta para o filho esquerdo do no a ser removido
@@ -141,38 +209,101 @@ pont removeNoh(pont raiz, TIPOCHAVE ch) {
     }
 
     //verifica se o que sera removido e a raiz
-    if(!pai){
+    if (pai == NULL)
+    {
+
+        int guarda = maisADireita->chave;
+        removeNoh(raiz, maisADireita->chave);
+        raiz->chave = guarda;
         //remove o noh desejado
-        free(no);
         //retorna o mais a direita
-        return maisADireita;
+        return raiz;
     }
 
-    if(ch < pai->chave) pai->esq = maisADireita;
-    else pai->dir = maisADireita;
+    if (ch < pai->chave)
+        pai->esq = maisADireita;
+    else
+        pai->dir = maisADireita;
 
     free(no);
 
     return raiz;
 }
 
-void main(){
+int balanco (pont r) {
+    pont atual = r;
+
+    int bal = altura(atual->dir) - altura(atual->esq);
+
+    if(bal <= 1 && bal >= -1) {
+        if(atual->esq != NULL) balanco(atual->esq);
+        if(atual->dir != NULL) balanco(atual->dir);
+        return 1;
+    }else return 0;
+}
+
+int balancoBinaria(pont r) {
+    pont atual = r;
+
+    printf("%d ", atual->chave);
+     if(((atual->esq == NULL) && (atual->dir == NULL)) 
+        ||  ((atual->esq == NULL) && (atual->dir->chave > atual->chave))
+            || ((atual->esq->chave < atual->chave) && (atual->dir == NULL))
+                || ((atual->esq->chave < atual->chave) && (atual->dir->chave > atual->chave))) {
+        int bal = altura(atual->dir) - altura(atual->esq);
+
+        if(bal <= 1 && bal >= -1) {
+            if(atual->esq != NULL) balancoBinaria(atual->esq);
+            if(atual->dir != NULL) balancoBinaria(atual->dir);
+            return 1;
+        }else return 0;
+    }else return 0;
+}
+
+void verificaBalanco(pont r){
+    int verifica = balancoBinaria(r);
+
+    if(verifica == 1) printf("Arvore balanceada e e uma arvore de busca");
+    else printf("Meu xibiu");
+}
+void main()
+{
 
     pont r = inicializarArvore();
-    pont noh = criaNovoNoh(23);
+    pont noh = criaNovoNoh(10);
     r = inserir(r, noh);
-    noh = criaNovoNoh(15);
-    r = inserir(r, noh);
-    noh = criaNovoNoh(28);
+    noh = criaNovoNoh(6);
     r = inserir(r, noh);
     noh = criaNovoNoh(2);
     r = inserir(r, noh);
+    noh = criaNovoNoh(8);
+    r = inserir(r, noh);
+    noh = criaNovoNoh(15);
+    r = inserir(r, noh);
+    noh = criaNovoNoh(12);
+    r = inserir(r, noh);
+    noh = criaNovoNoh(16);
+    r = inserir(r, noh);
+    noh = criaNovoNoh(11);
+    r = inserir(r, noh);
+    noh = criaNovoNoh(13);
+    r = inserir(r, noh);
+    
 
-    printf("%d\n", contagemNumeroNohs(r));
-    imprimeOrdem(r);
+    
+/*     imprimePreOrdem(r);
+    printf("\n");
+    removeNoh(r, 10);
+    printf("%d\n", contagemNumeroNohs(r)); */
+    //imprimeOrdem(r);
     printf("\n");
     imprimePreOrdem(r);
     printf("\n");
-    imprimePosOrdem(r);
-    
+    printf("%d ", altura(r));
+    //imprimePosOrdem(r);
+    printf("\n");
+    removeNoh(r, 15);
+    imprimePreOrdem(r);
+    //imprime_red(r);
+    //verificaBalanco(r);
 }
